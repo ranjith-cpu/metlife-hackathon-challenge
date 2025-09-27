6fd22c302ecfdd50838a102ee1d3c0806f4f58a7
abu-0099 <pattanabdullakhan@gmail.com>
2025-09-27 17:06:19 +0530

    preventive_plans_related_changes

--- src/main/java/com/metlife/arogyasutra/controller/ArogyaSutraController.java
+++ src/main/java/com/metlife/arogyasutra/controller/ArogyaSutraController.java
@@ -3,7 +3,11 @@ package com.metlife.arogyasutra.controller;
 import com.metlife.arogyasutra.model.Customer;
 import com.metlife.arogyasutra.model.CustomerRC;
 import com.metlife.arogyasutra.model.EmployeeHealthRecord;
+import com.metlife.arogyasutra.service.PreventivePlansService;
 import com.metlife.arogyasutra.service.RiskCalculatorService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +16,11 @@ import org.springframework.web.bind.annotation.*;
 public class ArogyaSutraController {
 
     private final RiskCalculatorService riskCalculatorService;
+    private PreventivePlansService preventivePlansService;
 
-    public ArogyaSutraController(RiskCalculatorService riskCalculatorService) {
+    public ArogyaSutraController(RiskCalculatorService riskCalculatorService, PreventivePlansService preventivePlansService) {
         this.riskCalculatorService = riskCalculatorService;
+        this.preventivePlansService = preventivePlansService;
     }
 
     @PostMapping("/customer/details")
@@ -29,5 +35,12 @@ public class ArogyaSutraController {
         CustomerRC score=riskCalculatorService.fetchRiskScores(employeeHealthRecord);
         return ResponseEntity.ok(score);
     }
+    
+    @GetMapping("/customer/preventivePlans")
+    public ResponseEntity<String> preventivePlans(@RequestParam String id)
+    {
+    	return new ResponseEntity<>(preventivePlansService.preventivePlanSuggestion(id),HttpStatus.OK);
+    }
+
 
 }
---
+++ src/main/java/com/metlife/arogyasutra/service/PreventivePlansService.java
@@ -0,0 +1,75 @@
+package com.metlife.arogyasutra.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import com.metlife.arogyasutra.repository.ArogyaSutraRepository;
+import com.metlife.arogyasutra.util.DataSourceConfig;
+
+@Component
+public class PreventivePlansService {
+
+	private ArogyaSutraRepository repo;
+	private DataSource dataSource;
+
+	@Autowired
+	public String preventivePlansService(DataSource dataSource) {
+		this.dataSource = dataSource;
+		return "hello";
+	}
+
+	public String preventivePlanSuggestion(String id)
+	{
+		String result = "";
+		boolean diabetic = false;
+		boolean heartProb = false;
+		JdbcTemplate temp = new JdbcTemplate(this.dataSource);
+		Map<String, Object> riskMap = new HashMap<>();
+		riskMap = temp.queryForMap("select riskScoreDiabetes1yr,riskScoreCvd1yr from EmployeeHealthRecord where id = "+id);
+		
+		float diabetsRiskScore = (float) riskMap.get("riskScoreDiabetes1yr");
+		float heatRiskScore = (float) riskMap.get("riskScoreDiabetes1yr");
+		if((diabetsRiskScore - 0) < (diabetsRiskScore - 1)){
+			
+		}
+		else {
+			diabetic = true;
+		}
+		if((heatRiskScore - 0) < (heatRiskScore - 1)){
+			
+		}
+		else {
+			heartProb = true;
+		}
+		
+		
+		if(diabetic)
+		{
+			result = "diabetic";
+		}
+		else if(heartProb)
+		{
+			result = "heartprob";
+		}
+		else if(diabetic&&heartProb)
+		{
+			result = "diabetic and heartprob";
+		}
+		else {
+			result = "normal";
+		}
+		
+		
+		
+		return result;
+	}
+
+}
