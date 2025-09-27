f413e0b26e94c70ba87fddb784be5097d77bd920
jaswanth <jaswanth.javvaji777@gmail.com>
2025-09-27 11:31:16 +0530

    Created package structure

--- src/main/java/com/metlife/arogyasutra/ServletInitializer.java
+++ src/main/java/com/metlife/arogyasutra/ServletInitializer.java
@@ -1,5 +1,6 @@
 package com.metlife.arogyasutra;
 
+import com.metlife.arogyasutra.application.ArogyasutraApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
--- src/main/java/com/metlife/arogyasutra/application/ArogyasutraApplication.java
+++ src/main/java/com/metlife/arogyasutra/application/ArogyasutraApplication.java
@@ -1,4 +1,4 @@
-package com.metlife.arogyasutra;
+package com.metlife.arogyasutra.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
